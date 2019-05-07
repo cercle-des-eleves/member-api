455af28a658376a91757bbb9cac2d586bc026c94
dodo <dorian.dignac@gmail.com>
2019-05-07 14:39:09 +0200

    add draft for group class, to show how it works

--- src/main/java/com/cercle/memberapi/business/service/GroupService.java
+++ src/main/java/com/cercle/memberapi/business/service/GroupService.java
@@ -1,6 +1,6 @@
 package com.cercle.memberapi.business.service;
 
-import com.cercle.memberapi.api.v1.model.GroupDTO;
+import com.cercle.memberapi.api.model.GroupDTO;
 
 public interface GroupService {
    GroupDTO getGroupWithID(String id);
