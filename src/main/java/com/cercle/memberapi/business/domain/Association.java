ebbf22f5201027d79ab2b7170c074491db44db17
dodo <dorian.dignac@gmail.com>
2019-05-07 14:07:34 +0200

    add draft for group class, to show how it works

---
+++ src/main/java/com/cercle/memberapi/business/domain/Association.java
@@ -0,0 +1,16 @@
+//package com.cercle.memberapi.business.domain;
+//
+//import lombok.Data;
+//
+//import java.time.ZonedDateTime;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Data
+//public class Association extends Group{
+//    List<Group> groups;
+//
+//    public Association(String id, String name, Boolean active, ZonedDateTime creationDate) {
+//        super(id, name, active, creationDate);
+//    }
+//}
---
+++ src/main/java/com/cercle/memberapi/business/domain/Club.java
@@ -0,0 +1,14 @@
+//package com.cercle.memberapi.business.domain;
+//
+//import lombok.Data;
+//
+//import java.time.ZonedDateTime;
+//import java.util.UUID;
+//
+//@Data
+//public class Club extends Group{
+//
+//    public Club(String id, String name, Boolean active, ZonedDateTime creationDate) {
+//        super(id, name, active, creationDate);
+//    }
+//}
