6025427c2dc33e17c3261de57de293f77ec5e8c6
dodo <dorian.dignac@gmail.com>
2019-05-07 14:07:34 +0200

    rebase to get properties

--- src/main/java/com/cercle/memberapi/business/domain/Group.java
+++ src/main/java/com/cercle/memberapi/business/domain/Group.java
@@ -3,23 +3,13 @@ package com.cercle.memberapi.business.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
 
 @AllArgsConstructor
-@Data
-@Entity
 public class Group {
-
-    @Id
-    @GeneratedValue(generator="system-uuid")
-    private UUID id;
-
-
+    private String id;
     private String name;
     private Boolean active;
     private ZonedDateTime creationDate;
