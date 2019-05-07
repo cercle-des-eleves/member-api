2c24384b93e6cbe02bb6f04ead571a1075a25057
dodo <dorian.dignac@gmail.com>
2019-05-07 14:07:34 +0200

    add draft for group class, to show how it works

---
+++ src/main/java/com/cercle/memberapi/business/domain/Board.java
@@ -0,0 +1,16 @@
+//package com.cercle.memberapi.business.domain;
+//
+//import lombok.Data;
+//
+//import java.time.ZonedDateTime;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@Data
+//public class Board extends Group{
+//    private List<Club> clubs;
+//
+//    public Board(String id, String name, Boolean active, ZonedDateTime creationDate) {
+//        super(id, name, active, creationDate);
+//    }
+//}
---
+++ src/main/java/com/cercle/memberapi/business/service/impl/GroupServiceImpl.java
@@ -0,0 +1,15 @@
+package com.cercle.memberapi.business.service.impl;
+
+import com.cercle.memberapi.api.v1.model.GroupDTO;
+import com.cercle.memberapi.business.service.GroupService;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+
+@Service
+public class GroupServiceImpl implements GroupService {
+    @Override
+    public GroupDTO getGroupWithID(String id) {
+        return new GroupDTO(id,"BDE",true, ZonedDateTime.now());
+    }
+}
