bc614d4f210ca15dde24c1210bf45267af95b708
Bastien <eataz19@gmail.com>
2019-05-09 15:46:53 +0200

    Add Service

--- src/main/java/com/cercle/memberapi/api/controller/StudentController.java
+++ src/main/java/com/cercle/memberapi/api/controller/StudentController.java
@@ -3,18 +3,16 @@ package com.cercle.memberapi.api.controller;
 import com.cercle.memberapi.api.model.StudentDTO;
 import com.cercle.memberapi.business.domain.Student;
 import com.cercle.memberapi.business.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 
 @Controller
 @RequestMapping(path = "/student")
 public class StudentController {
-
     private StudentService studentService;
 
     public StudentController(StudentService studentService) {
@@ -25,4 +23,5 @@ public class StudentController {
     public Student getStudents(){
         return new Student("12","Pouzeratte","Alexis", "alexis.pouzeratte@mines-ales.org","bgdu78", ZonedDateTime.now());
     }
+
 }
