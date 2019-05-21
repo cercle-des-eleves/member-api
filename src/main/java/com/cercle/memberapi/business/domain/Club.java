6baeba19409d89386d3186abe1c2f77d6fa583e8
dodo <dorian.dignac@gmail.com>
2019-05-21 11:33:34 +0200

    Cas d'utilisation Group pas fini mais fonctionnel

--- src/main/java/com/cercle/memberapi/business/domain/Club.java
+++ src/main/java/com/cercle/memberapi/business/domain/Club.java
@@ -1,14 +1,15 @@
-//package com.cercle.memberapi.business.domain;
-//
-//import lombok.Data;
-//
-//import java.time.ZonedDateTime;
-//import java.util.UUID;
-//
-//@Data
-//public class Club extends Group{
-//
-//    public Club(String id, String name, Boolean active, ZonedDateTime creationDate) {
-//        super(id, name, active, creationDate);
-//    }
-//}
+package com.cercle.memberapi.business.domain;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Club extends Group {
+
+    public Club(UUID id, String name, Boolean active, ZonedDateTime creationDate) {
+        super(id, name, active, creationDate);
+    }
+}
---
+++ src/main/java/com/cercle/memberapi/persistence/repository/GroupRepository.java
@@ -0,0 +1,15 @@
+package com.cercle.memberapi.persistence.repository;
+
+import com.cercle.memberapi.business.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, UUID> {
+
+    @Override
+    Optional<Group> findById(UUID uuid);
+}
