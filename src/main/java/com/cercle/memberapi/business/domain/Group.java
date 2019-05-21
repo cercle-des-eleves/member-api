cfbb897f04a83248f9d37e328e2932d5864cb0f7
dodo <dorian.dignac@gmail.com>
2019-05-21 11:33:34 +0200

    Cas d'utilisation Group pas fini mais fonctionnel

--- src/main/java/com/cercle/memberapi/business/domain/Group.java
+++ src/main/java/com/cercle/memberapi/business/domain/Group.java
@@ -3,13 +3,23 @@ package com.cercle.memberapi.business.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
 
 @AllArgsConstructor
+@Data
+@Entity
 public class Group {
-    private String id;
+
+    @Id
+    @GeneratedValue(generator="system-uuid")
+    private UUID id;
+
+
     private String name;
     private Boolean active;
     private ZonedDateTime creationDate;
