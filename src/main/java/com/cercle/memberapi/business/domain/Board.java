9b4e6249b008449622b858849e633034a74316c5
dodo <dorian.dignac@gmail.com>
2019-05-21 11:33:34 +0200

    Cas d'utilisation Group pas fini mais fonctionnel

--- src/main/java/com/cercle/memberapi/business/domain/Board.java
+++ src/main/java/com/cercle/memberapi/business/domain/Board.java
@@ -1,16 +1,21 @@
-//package com.cercle.memberapi.business.domain;
-//
-//import lombok.Data;
-//
-//import java.time.ZonedDateTime;
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Data
-//public class Board extends Group{
-//    private List<Club> clubs;
-//
-//    public Board(String id, String name, Boolean active, ZonedDateTime creationDate) {
-//        super(id, name, active, creationDate);
-//    }
-//}
+package com.cercle.memberapi.business.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class Board extends Group{
+
+    private List<Group> clubs;
+
+    public Board(UUID id, String name, Boolean active, ZonedDateTime creationDate, List<Group> clubs) {
+        super(id, name, active, creationDate);
+        this.clubs = clubs;
+    }
+
+}
