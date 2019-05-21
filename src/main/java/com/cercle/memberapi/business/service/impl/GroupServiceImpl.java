cfc0cf5e574a778d97c18f6c886ac5d3ee50b88d
dodo <dorian.dignac@gmail.com>
2019-05-21 11:34:33 +0200

    Cas d'utilisation Group pas fini mais fonctionnel

--- src/main/java/com/cercle/memberapi/business/service/impl/GroupServiceImpl.java
+++ src/main/java/com/cercle/memberapi/business/service/impl/GroupServiceImpl.java
@@ -1,15 +1,32 @@
 package com.cercle.memberapi.business.service.impl;
 
+import com.cercle.memberapi.api.GroupMapper;
 import com.cercle.memberapi.api.model.GroupDTO;
+import com.cercle.memberapi.business.domain.Group;
 import com.cercle.memberapi.business.service.GroupService;
+import com.cercle.memberapi.persistence.repository.GroupRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class GroupServiceImpl implements GroupService {
+
+    private GroupMapper groupMapper;
+    private GroupRepository groupRepository;
+
+    public GroupServiceImpl(GroupMapper groupMapper, GroupRepository groupRepository) {
+        this.groupMapper = groupMapper;
+        this.groupRepository = groupRepository;
+    }
+
     @Override
     public GroupDTO getGroupWithID(String id) {
-        return new GroupDTO(id,"BDE",true, ZonedDateTime.now());
+
+        Optional<Group> result = groupRepository.findById(UUID.fromString(id));
+
+        return result.map(group -> groupMapper.toGroupDTO(group)).orElseThrow(() -> new RuntimeException("Group Not Found"));
     }
 }
