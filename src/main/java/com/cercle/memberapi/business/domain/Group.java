4b8e4ad794439aa4dd6ddb2068fbc22d3c964e47
dorian <doria.dignac@mines-ales.org>
2019-06-27 11:27:22 +0200

    trop de trucs

--- src/main/java/com/cercle/memberapi/business/domain/Group.java
+++ src/main/java/com/cercle/memberapi/business/domain/Group.java
@@ -6,22 +6,29 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.UUID;
 
 
 @AllArgsConstructor
 @Data
-@Entity(name = "`group`")
+@Entity(name = "`Group`")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING, name = "group_type")
 public class Group {
 
     @Id
-    @Column(length = 128)
+    @Column(length = 128,updatable = false, nullable = false)
     @GeneratedValue(generator="system-uuid")
     @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id;
 
-
     private String name;
     private Boolean active;
     private ZonedDateTime creationDate;
+
+    //private List<Member> members ;
 }
+
+
+
