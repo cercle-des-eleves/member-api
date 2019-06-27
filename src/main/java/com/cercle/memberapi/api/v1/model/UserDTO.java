c0e0a0cdce6f118a0c0535443bb6423d630bf966
dorian <doria.dignac@mines-ales.org>
2019-06-27 19:41:36 +0200

    Changmenet Student Par USer

---
+++ src/main/java/com/cercle/memberapi/api/v1/model/UserDTO.java
@@ -0,0 +1,19 @@
+package com.cercle.memberapi.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String id;
+    private String lastName;
+    private String firstName;
+    private String mail;
+    private String password;
+    private ZonedDateTime creationDate;
+}
