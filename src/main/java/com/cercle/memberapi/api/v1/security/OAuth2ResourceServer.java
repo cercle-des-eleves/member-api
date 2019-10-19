190169d60774444d8a5f879d0fbcd32aadad02f4
dorian <doria.dignac@mines-ales.org>
2019-10-19 17:23:50 +0100

    init with test classes

---
+++ src/main/java/com/cercle/memberapi/api/v1/security/OAuth2ResourceServer.java
@@ -0,0 +1,20 @@
+package com.cercle.memberapi.api.v1.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class OAuth2ResourceServer extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
