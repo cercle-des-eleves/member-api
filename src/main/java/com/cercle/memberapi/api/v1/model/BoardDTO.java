36ece41d307bb50ae43398867b979e565e57cd1d
Guitou <guillaume.lagay@etu.univ-amu.fr>
2020-01-12 16:53:19 +0100

    - refactor : added BoardDTO & BoardDetailledDTO

---
+++ src/main/java/com/cercle/memberapi/api/v1/model/BoardDTO.java
@@ -0,0 +1,21 @@
+package com.cercle.memberapi.api.v1.model;
+
+import com.cercle.memberapi.business.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardDTO {
+    private String id;
+    private String name;
+    private Boolean active;
+    private ZonedDateTime creationDate;
+    private Member president;
+
+}
+
---
+++ src/main/java/com/cercle/memberapi/api/v1/model/BoardDetailedDTO.java
@@ -0,0 +1,7 @@
+package com.cercle.memberapi.api.v1.model;
+
+import java.util.List;
+
+public class BoardDetailedDTO extends BoardDTO {
+    private List<ClubDTO> clubs;
+}
